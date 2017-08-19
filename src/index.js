d17fe877960b8749ce084140ed3d767efb9f61a6
Ivan HA <hangolam@gmail.com>
2017-08-19 20:21:16 +0800

    [infra] basic repo setup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
-import reducer from './reducers';
-
 import App from './components/app'
 
-const store = createStore(reducer, applyMiddleware());
-
-const AppContainter = () => {
-    return (
-        <Provider store={store}>
-            <App />
-        </Provider>
-    );
-};
-
-ReactDOM.render(<AppContainter />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
